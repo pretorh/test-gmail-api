1b5caf25f3e1e9bbd3ba2280910abef4232b3c3d
Hendri Pretorius <pretorh@gmail.com>
2016-04-14 06:48:28 +0200

    move auth functions into lib/auth

--- quickstart.js
+++ quickstart.js
@@ -1,86 +1,37 @@
-var fs = require('fs');
-var readline = require('readline');
+var auth = require('./lib/auth');
+
 var google = require('googleapis');
-var googleAuth = require('google-auth-library');
 var util = require('util');
 
 var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 var TOKEN_DIR = '.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'gmail-nodejs-quickstart.json';
 
-fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
+auth.readClientSecret('client_secret.json', function processClientSecrets(err, secret) {
+    if (err) {
+        console.log('Error loading client secret file: ' + err);
+        return;
+    }
 
-  authorize(JSON.parse(content), function(auth) {
-      if (process.argv[2] === 'threads') {
-          listThreads(auth, process.argv[3]);
-      } else if (process.argv[2] === 'list-labels') {
-          listLabels(auth, process.argv[3]);
-      } else if (process.argv[2] === 'message-full') {
-          getMessage(auth, 'full', process.argv[3]);
-      } else if (process.argv[2] === 'message') {
-          getMessage(auth, 'minimal', process.argv[3]);
-      } else {
-          console.error('invalid command');
-      }
-  });
+    var cache = {
+        dir: TOKEN_DIR,
+        path: TOKEN_PATH,
+    };
+    auth.authorize(secret, SCOPES, cache, authorized);
 });
 
-function authorize(credentials, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var auth = new googleAuth();
-  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, callback);
+function authorized(auth) {
+    if (process.argv[2] === 'threads') {
+        listThreads(auth, process.argv[3]);
+    } else if (process.argv[2] === 'list-labels') {
+        listLabels(auth, process.argv[3]);
+    } else if (process.argv[2] === 'message-full') {
+        getMessage(auth, 'full', process.argv[3]);
+    } else if (process.argv[2] === 'message') {
+        getMessage(auth, 'minimal', process.argv[3]);
     } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client);
+        console.error('invalid command');
     }
-  });
-}
-
-function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
-      }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
-    });
-  });
-}
-
-function storeToken(token) {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
-    }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-  console.log('Token stored to ' + TOKEN_PATH);
 }
 
 function listLabels(auth, match) {
